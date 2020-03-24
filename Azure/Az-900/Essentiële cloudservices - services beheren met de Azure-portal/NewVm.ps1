37cb05516a700f1ba233e57a2b0205b3d52c4693
WtotdeD <wouter.dijk.e@gmail.com>
2020-03-24 12:00:40 +0100

    Added some structure to the az module

---
+++ Azure/Az-900/Essentiële cloudservices - services beheren met de Azure-portal/NewVm.ps1
@@ -0,0 +1,8 @@
+New-AzVm `
+    -ResourceGroupName "TestResourceGroup" `
+    -Name "test-wp1-eus-vm" `
+    -Location "East US" `
+    -VirtualNetworkName "test-wp1-eus-network" `
+    -SubnetName "default" `
+    -SecurityGroupName "test-wp1-eus-nsg" `
+    -PublicIpAddressName "test-wp1-eus-pubip" `
